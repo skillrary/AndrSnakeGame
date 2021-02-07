a466ceb7474a18aa1a017987a31427db71e786bf
Mithun Ashok <developer@skillrary.com>
2021-02-07 13:25:55 +0530

    Modified Folders

--- app/src/androidTest/java/com/example/krisnayana/snakegame/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/krisnayana/snakegame/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.krisnayana.snakegame", appContext.getPackageName());
+        assertEquals("com.example.mithunashok.snakegame", appContext.getPackageName());
     }
 }
