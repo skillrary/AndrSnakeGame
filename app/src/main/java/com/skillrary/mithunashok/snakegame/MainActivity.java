064757a73fe0a3b5a64b9c86d03870f4375d8096
Mithun Ashok <developer@skillrary.com>
2021-02-08 15:16:10 +0530

    Android X

--- app/src/main/java/com/skillrary/mithunashok/snakegame/MainActivity.java
+++ app/src/main/java/com/skillrary/mithunashok/snakegame/MainActivity.java
@@ -9,7 +9,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.KeyEvent;
